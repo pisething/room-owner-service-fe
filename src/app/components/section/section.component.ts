3787393a394a0a819faa08a878fa0c04591e5e09
pisething <piseth.ing2022@gmail.com>
2025-11-16 09:26:35 +0700

    init commit

---
+++ src/app/components/section/section.component.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+import { PropertiesHeaderComponent } from "../properties-header/properties-header.component";
+import { PropertiesGridComponent } from "../properties-grid/properties-grid.component";
+import { PropertiesListComponent } from "../properties-list/properties-list.component";
+import { PaginationComponent } from "../pagination/pagination.component";
+import { PropertiesSidebarComponent } from "../properties-sidebar/properties-sidebar.component";
+import { RoomListParams } from '../../models/room-list-params';
+import { RoomFormComponent } from "../room-form/room-form.component";
+
+@Component({
+  selector: 'app-section',
+  imports: [PropertiesHeaderComponent, PropertiesGridComponent, PropertiesListComponent, PaginationComponent, PropertiesSidebarComponent, RoomFormComponent],
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.css'
+})
+export class SectionComponent {
+
+  filter = signal<RoomListParams>({page: 0, size: 4, priceMin: null, priceMax: null});
+
+  onFilterChanged(f: RoomListParams){
+    this.filter.set(f);
+  }
+}
