1dc6b9c00e5b0b7094f134f2b1f9c3e925d2dc98
pisething <piseth.ing2022@gmail.com>
2025-11-30 14:35:43 +0700

    ui improvement

--- src/app/components/room-edit/room-edit.component.ts
+++ src/app/components/room-edit/room-edit.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Room } from '../../models/room';
 import { RoomService } from '../../services/room.service';
 import { RoomFormComponent } from "../room-form/room-form.component";
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-room-edit',
@@ -14,6 +15,7 @@ export class RoomEditComponent {
   private roomService = inject(RoomService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
+  private snackBar = inject(MatSnackBar);
 
   // strictly typed signal
   room = signal<Room | null>(null);
@@ -39,11 +41,23 @@ export class RoomEditComponent {
   onUpdate(evt: { id: string; body: Room }) {
     this.roomService.update(evt.id, evt.body).subscribe({
       next: () => {
+
+        this.snackBar.open('Room updated successfully', 'Close', {
+          duration: 3000,
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom'
+        });
+
         this.router.navigate(['/rooms', evt.id]);
       },
       error: err => {
         console.error('Update failed', err);
-        alert('Failed to update room.');
+        
+        this.snackBar.open('Failed to update room', 'Close', {
+          duration: 4000,
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom'
+        });
       }
     });
   }
